77dc03fe3ee00b219818b87320a428d063e590aa
ANJANA A P <anjanaaravind2793@gmail.com>
2026-05-27 15:47:26 +0530

    Time: 11 ms (57.96%) | Memory: 60 MB (80.74%) - LeetSync

---
+++ 557-reverse-words-in-a-string-iii/reverse-words-in-a-string-iii.js
@@ -0,0 +1,13 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseWords = function(s) {
+     let word = s.split(" ");   
+    let result = "";
+    for (let i=0;i<word.length;i++) {
+        result += word[i].split("").reverse().join("") + " ";
+    }
+
+    return result.trim();
+};
